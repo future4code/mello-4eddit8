2ea0ab3fcb63f57b7c26b482933d9123bdfff8bb
Kaueny Alves <63555634+Kaueny-Alves@users.noreply.github.com>
2020-07-07 12:07:28 -0300

    App

---
+++ projeto-4eddit8/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginPage from './Components/LoginPage';
+import HomePage from './Components/HomePage';
+
+
+function App() {
+  return (
+    <div >
+     
+    </div>
+  );
+}
+
+export default App;
