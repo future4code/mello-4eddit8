c40e588009260a767d1d3545a37fcb66e19c0633
Paulo Aguiar <paulopma@hotmail.com>
2020-07-09 16:21:55 -0300

    lista de posts funcionando na timeline

--- projeto-4eddit8/src/Components/FeedPage/FeedPage.js
+++ projeto-4eddit8/src/Components/FeedPage/FeedPage.js
@@ -1,24 +1,76 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { StylesProvider } from '@material-ui/core/styles';
 import PostsCard from '../PostsCard/PostsCard'
+import axios from 'axios'
+import {
+  Main,
+  InputNewPost,
+  ButtonNewPost
+} from './style'
 
 function PostsPage() {
-  const newPost = {
-    yuzo: 'Paulo Aguiar',
-    text: 'Este é um novo post',
-    votes: 2,
-    numberOfComments: 3
+  const [postList, setPostList] = useState([])
+
+  useEffect(()=> {
+    const userToken = localStorage.getItem("token");
+    console.log(userToken)
+    getPosts()
+  }, [])
+
+  function getPosts() {
+    const userToken = localStorage.getItem("token");
+    const headers = {
+      Authorization: userToken
+    }
+    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labEddit/posts', {headers})
+      .then((response) => {
+        setPostList(response.data.posts)
+      })
+      .catch((error) => {
+        window.alert(error)
+      })
+  }
+
+  function upVote() {
+    window.alert('upvote!!')
+  }
+
+  function downVote() {
+    window.alert('downvote!!')
   }
 
   return (
-    <main>
-      <h2>Página de Feed</h2>
-      <PostsCard 
-        user={newPost.yuzo}
-        text={newPost.text}
-        commentsCount={newPost.numberOfComments}
-        votesCount={newPost.votes}
-      />
-    </main>
+    <StylesProvider injectFirst>
+      <Main>
+        <h2>Página de Feed</h2>
+        <form>
+          <InputNewPost 
+            placeholder='quais as novidades?' 
+            variant='outlined' 
+            multiline
+            rows={3}
+          />
+          <ButtonNewPost 
+            color='primary' 
+            variant='outlined'>
+            Postar
+          </ButtonNewPost>
+        </form>
+        {postList.length === 0 ? <p>Carregando Posts...</p> : postList.map((post) => {
+          return (
+          <section>
+            <PostsCard 
+              upVote={upVote}
+              downVote={downVote}
+              user={post.username}
+              text={post.text}
+              commentsCount={post.commentsCount}
+              votesCount={post.votesCount}
+            />
+          </section>)
+        })}
+      </Main>
+    </StylesProvider>
   )
 }
 
