b3746f8f0172d89fce24aecea3e72e3b4999e35a
Rafael  Luis Alves <rafaelalves@MacBook-Pro-de-Rafael.local>
2020-07-09 14:43:41 -0300

    Redirecionameto para as paginas

--- projeto-4eddit8/src/Hooks/useForm.js
+++ projeto-4eddit8/src/Hooks/useForm.js
@@ -1,14 +1,11 @@
-//import { useState } from "react"
+//  import { useState } from "react"
 //
-//export const useForm = () => {
+//   export const useForm = () => {
 //
-//    const [form, setForm]= useState()
+//   const [form, setForm]= useState()
 //
 //    const onchange = (event) => {
-//        const newValue = event.target.value
-//        const fieldName = event.target.name
-//
-//        setForm({[fieldName]: newValue})
-//    }
+//      setForm(event.target.value)
+//     }
 //    return [form, onchange]
-//}
+//  }
